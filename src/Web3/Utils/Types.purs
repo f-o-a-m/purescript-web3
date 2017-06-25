690338f91879a2d679fd9373594306a2651a9668
Martin Allen <martin.allen26@gmail.com>
2017-06-25 11:05:09 -0400

    transformToFullName

--- src/Web3/Utils/Types.purs
+++ src/Web3/Utils/Types.purs
@@ -1,6 +1,10 @@
 module Web3.Utils.Types where
 
 import Prelude
+import Data.List (List)
+--------------------------------------------------------------------------------
+-- * HexString
+--------------------------------------------------------------------------------
 
 newtype HexString = HexString String
 
@@ -8,3 +12,35 @@ instance showHexString :: Show HexString where
   show (HexString hx) = "0x" <> hx
 
 derive newtype instance hexStringEq :: Eq HexString
+
+--------------------------------------------------------------------------------
+-- * Contract Interface and Event Description
+--------------------------------------------------------------------------------
+
+data AbiElement = FunctionType | Event
+
+type FunctionType = { type_ :: FunctionClass
+                    , name :: String
+                    , inputs :: List FunctionInputs
+                    , constant :: Boolean
+                    , payable :: Boolean
+                    }
+
+data FunctionClass =
+    Function
+  | Constructor
+  | Fallback
+
+type FunctionInputs = { name :: String
+                      , type_ :: String
+                      }
+
+type Event = { name :: String
+             , inputs :: List EventInputs
+             , anonymous :: Boolean
+             }
+
+type EventInputs = { name :: String
+                   , type_ :: String
+                   , indexed :: Boolean
+                   }
