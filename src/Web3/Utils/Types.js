7b94896b1558547e3648dd411d7da643228a3674
James Pak <blinky3713@gmail.com>
2017-06-27 23:30:00 -0400

    bignum algebra

--- src/Web3/Utils/Types.js
+++ src/Web3/Utils/Types.js
@@ -1,9 +1,30 @@
 var BigNumber = require('bignumber.js');
 
-exports.numberToBigNumber = function(value) {
+exports._intToBigNumber = function(value) {
   return new BigNumber(value.toString(10), 10);
 };
 
-exports._hexStringToBigNumber = function(value) {
-  return new BigNumber(value, 16);
+exports._showBigNumber = function (n) {
+    return "0x".concat(BigNumber.toString(n, 16));
 };
+
+exports._eqBigNumber = function(n) {
+  return function(m) { return n.toString == m.toString; };
+};
+
+exports._addBigNumber = function(n) {
+    return function (m) { return n.add(m); };
+};
+
+exports._mulBigNumber = function(n) {
+    return function (m) { n.times(m); };
+};
+
+exports._subBigNumber = function(n) {
+    return function (m) { n.plus(m); };
+};
+
+exports._zero = new BigNumber(0);
+
+exports._one = new BigNumber(1, 16);
+
--- src/Web3/Utils/Types.purs
+++ src/Web3/Utils/Types.purs
@@ -1,7 +1,10 @@
 module Web3.Utils.Types where
 
+import Data.Ring
 import Prelude
+
 import Data.List (List)
+
 --------------------------------------------------------------------------------
 -- * HexString
 --------------------------------------------------------------------------------
@@ -19,12 +22,52 @@ derive newtype instance hexStringEq :: Eq HexString
 
 foreign import data BigNumber :: Type
 
-foreign import numberToBigNumber :: Number -> BigNumber
+foreign import _showBigNumber :: BigNumber -> String
 
-foreign import _hexStringToBigNumber :: String -> BigNumber
+foreign import _eqBigNumber :: BigNumber -> BigNumber -> Boolean
 
-hexStringToBigNumber :: HexString -> BigNumber
-hexStringToBigNumber (HexString hx) = _hexStringToBigNumber hx
+instance showBigNumber :: Show BigNumber where
+  show = _showBigNumber
+
+instance eqBigNumber :: Eq BigNumber where
+  eq = _eqBigNumber
+
+foreign import _addBigNumber :: BigNumber -> BigNumber -> BigNumber
+
+foreign import _mulBigNumber :: BigNumber -> BigNumber -> BigNumber
+
+foreign import _zero :: BigNumber
+
+foreign import _one :: BigNumber
+
+
+
+instance semiringBigNumber :: Semiring BigNumber where
+  add = _addBigNumber
+  mul = _mulBigNumber
+  zero = _zero
+  one = _one
+
+foreign import _subBigNumber :: BigNumber -> BigNumber -> BigNumber
+
+instance ringBigNumber :: Ring BigNumber where
+  sub = _subBigNumber
+
+foreign import _intToBigNumber :: Int -> BigNumber
+
+class RModule r m where
+  rmul :: r -> m -> m
+  lmul :: m -> r -> m
+
+infixr 5 rmul as *<
+infixl 5 append as >*
+
+embedInt :: Int -> BigNumber
+embedInt = _intToBigNumber
+
+instance rmoduleIntBigNumber :: RModule Int BigNumber where
+  rmul r m = (embedInt r) * m
+  lmul m r = m * (embedInt r)
 
 --------------------------------------------------------------------------------
 -- * Decimal
