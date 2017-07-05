17395051e4e406c06e82cc38ed0976f7c471b126
James Pak <blinky3713@gmail.com>
2017-07-05 00:50:50 -0400

    added formatInputString

--- src/Web3/Solidity/Formatters.purs
+++ src/Web3/Solidity/Formatters.purs
@@ -6,7 +6,7 @@ import Data.Maybe (Maybe(Nothing))
 import Web3.Solidity.Param (SolidityParam(..), paramValue)
 import Web3.Utils.BigNumber (BigNumber, embed, toSignedHexString, toTwosComplement)
 import Web3.Utils.Types (HexString, asSigned, length)
-import Web3.Utils.Utils (padLeft, padRight)
+import Web3.Utils.Utils (padLeft, padRight, fromUtf8)
 
 formatInputInt :: BigNumber -> SolidityParam
 formatInputInt v =
@@ -24,3 +24,11 @@ formatDynamicInputBytes hx =
       l = (length hx + 63) `div` 64
       res = padRight (asSigned hx) (l * 64)
   in SolidityParam {value : (paramValue <<< formatInputInt $ len) <> res, offset : Nothing}
+
+formatInputString :: String -> SolidityParam
+formatInputString s =
+  let res = fromUtf8 s
+      len = embed $ length res `div` 2
+      l = (length res + 63)  `div` 64
+      result = padRight (asSigned res) (l * 64)
+  in SolidityParam {value : (paramValue <<< formatInputInt $ len) <> result, offset : Nothing}
