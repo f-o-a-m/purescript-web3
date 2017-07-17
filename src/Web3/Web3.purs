cba8f6ca0fafb6bbd8c2fe3594c303a9526023b5
Martin Allen <martin.allen26@gmail.com>
2017-07-17 11:24:54 -0400

    get balance, isConnected work

--- src/Web3/Web3.purs
+++ src/Web3/Web3.purs
@@ -1,11 +1,12 @@
 module Web3.Web3 where
 
 import Prelude
+import Control.Monad.Reader.Class (class MonadAsk, ask)
 import Control.Monad.Reader.Trans (ReaderT, runReaderT)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Control.Monad.Eff (kind Effect, Eff)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
-import Data.Function.Uncurried (Fn2, runFn2)
+import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, runEffFn1, runEffFn2)
 
 import Web3.Utils.Types (Address, BlockNumber)
 import Web3.Utils.BigNumber (BigNumber)
@@ -16,7 +17,7 @@ import Web3.Utils.BigNumber (BigNumber)
 --------------------------------------------------------------------------------
 foreign import data Web3 :: Type
 
-foreign import newWeb3 :: String -> Web3
+foreign import newWeb3 :: forall eff . EffFn1 (eth :: ETH | eff) String Web3
 
 --------------------------------------------------------------------------------
 -- * Web3M
@@ -26,13 +27,21 @@ foreign import data ETH :: Effect
 
 data Web3M m a = Web3M (ReaderT Web3 m a)
 
-runWeb3 :: forall m a . Web3M m a -> ReaderT Web3 m a
-runWeb3 (Web3M action) = action
+foreign import web3ShowImpl :: Web3 -> String
 
-execWeb3 :: String -> (forall m a. Monad m => Web3M m a -> m a)
-execWeb3 provider = \(Web3M action) ->
-  let web3 = newWeb3 provider
-  in runReaderT action web3
+instance showWeb3 :: Show Web3 where
+  show = web3ShowImpl
+
+unWeb3 :: forall m a . Web3M m a -> ReaderT Web3 m a
+unWeb3 (Web3M action) = action
+
+execWeb3 :: forall eff m a . MonadEff (eth :: ETH | eff) m
+         => String
+         -> Web3M m a
+         -> m a
+execWeb3 provider = \(Web3M action) -> do
+  web3 <- liftEff $ runEffFn1 newWeb3 provider
+  runReaderT action web3
 
 -- instances
 
@@ -46,21 +55,26 @@ instance web3MApplicative :: Applicative m => Applicative (Web3M m) where
   pure = Web3M <<< pure
 
 instance web3MBind :: Bind m => Bind (Web3M m) where
-  bind (Web3M m) f = Web3M (m >>= (runWeb3 <<< f))
+  bind (Web3M m) f = Web3M (m >>= (unWeb3 <<< f))
 
 instance web3MMonad :: (Applicative m, Bind m) => Monad (Web3M m)
 
 instance web3MEff :: MonadEff eff m => MonadEff eff (Web3M m) where
   liftEff = Web3M <<< liftEff
 
+instance web3Reader :: Monad m => MonadAsk Web3 (Web3M m) where
+  ask = Web3M ask
+
 --------------------------------------------------------------------------------
 -- * Eth
 --------------------------------------------------------------------------------
 
 getBalance :: forall eff m. MonadEff (eth :: ETH | eff) m => Address -> BlockNumber -> Web3M m BigNumber
-getBalance addr bn = runFn2 _getBalance addr (show bn)
+getBalance addr bn = do
+  web3 <- ask
+  liftEff $ runEffFn2 (_getBalance web3) addr (show bn)
 
-foreign import _getBalance :: forall m . Fn2 Address String (Web3M m BigNumber)
+foreign import _getBalance :: forall eff . Web3 -> EffFn2 (eth :: ETH | eff) Address String BigNumber
 
 verboseGetBalance :: forall eff .
                       Address
@@ -70,3 +84,10 @@ verboseGetBalance addr bn = do
   b <- getBalance addr bn
   liftEff $ logShow <<< show $ b
   pure b
+
+isConnected :: forall eff m. MonadEff (eth :: ETH | eff) m => Web3M m Boolean
+isConnected = do
+  web3 <- ask
+  liftEff $ runEffFn1 _isConnected web3
+
+foreign import _isConnected :: forall eff . EffFn1 (eth :: ETH | eff) Web3 Boolean
