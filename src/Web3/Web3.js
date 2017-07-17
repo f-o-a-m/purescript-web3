116e5692b75c39453528f53042e014994ad36ce6
James Pak <blinky3713@gmail.com>
2017-07-17 11:24:54 -0400

    get balance, isConnected work

--- src/Web3/Web3.js
+++ src/Web3/Web3.js
@@ -3,9 +3,18 @@
 var Web3 = require('web3');
 
 exports._getBalance = function(web3) {
-    return web3.eth.getBalance;
+  return web3.eth.getBalance;
 };
 
 exports.newWeb3 = function (provider) {
-    return new Web3(provider);
+    return new Web3(new Web3.providers.HttpProvider(provider));
+};
+
+exports._isConnected = function (web3) {
+    return web3.isConnected;
+};
+
+exports.web3ShowImpl = function (web3) {
+    var host = web3.currentProvider.host;
+    return "Connected to rpc node at: " + host;
 };
