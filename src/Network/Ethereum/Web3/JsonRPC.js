9c67d0491081cf13c4abe795a6dda88f40681f94
Martin Allen <martin.allen26@gmail.com>
2017-08-05 12:33:42 -0400

    async

--- src/Network/Ethereum/Web3/JsonRPC.js
+++ src/Network/Ethereum/Web3/JsonRPC.js
@@ -6,4 +6,16 @@
 //    provider.sendAsync(args)
 //};
 
+exports._sendAsync = function (callback) {
+    return function (provider) { // accepts a callback
+        return function(request) { // and a request
+            return function() { // returns an effect
+                provider.sendAsync(request, function(response) {
+                    callback(response)(); // callback itself returns an effect
+                });
+            };
+        };
+    };
+};
+
 exports.web3Request = 2;
--- src/Network/Ethereum/Web3/JsonRPC.purs
+++ src/Network/Ethereum/Web3/JsonRPC.purs
@@ -8,9 +8,10 @@ import Data.Foreign (Foreign)
 import Data.Foreign.Class (class Decode, class Encode, decode, encode)
 import Data.Generic.Rep (class Generic)
 import Data.Foreign.Generic (defaultOptions, genericEncode)
-import Control.Monad.Aff (Aff)
+import Control.Monad.Aff (Aff, makeAff)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Exception (error)
+import Control.Monad.Eff.Exception (EXCEPTION, error)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except (runExcept)
 
@@ -20,23 +21,24 @@ import Network.Ethereum.Web3.Types (ETH, Web3M(..))
 
 type MethodName = String
 
+
 class Remote e a where
-  remote_ :: (Provider -> Array Foreign -> Aff (eth :: ETH | e) Foreign) -> a
+  remote_ :: (Provider -> Array Foreign -> Eff (eth :: ETH, exception :: EXCEPTION | e) Foreign) -> a
 
 instance remoteBase :: Decode a => Remote e (Web3M e a) where
   remote_ f = do
     p <- liftEff getProvider
     res <- Web3M $ f p mempty
-    case runExcept <<< decode $ res of
-      -- TODO: fix bad error handling
+    let decoded = runExcept <<< decode $ res
+    case decoded of
       Left e -> throwError <<< error <<< show $ e
-      Right r -> pure r
+      Right a -> pure a
 
 instance remoteInductive :: (Encode a, Remote e b) => Remote e (a -> b) where
   remote_ f x = remote_ $ \p args -> f p (encode x : args)
 
 
-foreign import web3Request :: Provider -> Request ->  Aff (eth :: ETH) Foreign
+foreign import web3Request :: Provider -> Request ->  Eff (eth :: ETH, exception :: EXCEPTION) Foreign
 
 -- | Remote call of JSON-RPC method.
 -- Arguments of function are stored into @params@ request array.
@@ -44,6 +46,23 @@ foreign import web3Request :: Provider -> Request ->  Aff (eth :: ETH) Foreign
 remote :: forall a . Remote () a => MethodName -> a
 remote n = remote_ $ \provider ps -> web3Request provider $ mkRequest n 1 ps
 
+foreign import _sendAsync :: forall e . (Foreign -> Eff (eth :: ETH | e) Unit) -> Provider -> Request -> Eff (eth :: ETH | e) Unit
+
+remoteAsync :: forall e a . Decode a => Provider -> Request -> Aff (eth :: ETH | e) a
+remoteAsync p req = do
+    res <- makeAff (\error succ -> _sendAsync succ p req)
+    case runExcept <<< decode $ res of
+      -- TODO: fix bad error handling
+      Left e -> throwError <<< error <<< show $ e
+      Right r -> pure r
+
+remoteAsync' :: forall a . Remote () a => MethodName -> a
+remoteAsync' = do
+    res <- makeAff (\error succ -> _sendAsync succ p req)
+    case runExcept <<< decode $ res of
+      -- TODO: fix bad error handling
+      Left e -> throwError <<< error <<< show $ e
+      Right r -> pure r
 
 data Request =
   Request { jsonrpc :: String
