e24aed8463f7ad50a15001c4e8ed19e88fbed416
James Pak <blinky3713@gmail.com>
2017-08-07 09:04:08 -0400

    sendTransaction and call

--- src/Network/Ethereum/Web3/Api.purs
+++ src/Network/Ethereum/Web3/Api.purs
@@ -1,11 +1,13 @@
 module Network.Ethereum.Web3.Api where
 
 
-import Network.Ethereum.Web3.Types (Web3M, Address, BigNumber, Block, CallMode, HexString, Transaction)
+import Network.Ethereum.Web3.Types (Web3M, Web3MA, Address, BigNumber, Block, CallMode, HexString, Transaction, TransactionOptions)
 import Network.Ethereum.Web3.JsonRPC (remote)
 
 
 type Web3 a = Web3M () a
+
+type Web3A a = Web3MA () a
 
 -- | Returns current node version string.
 web3_clientVersion :: Web3 String
@@ -25,4 +27,14 @@ eth_getBlockByHash :: HexString -> Web3 Block
 eth_getBlockByHash = remote "eth_getBlockByHash"
 
 eth_getTransaction :: HexString -> Web3 Transaction
-eth_getTransaction = remote "eth_getTransaction"
+eth_getTransaction = remote "eth_getTransactionByHash"
+
+-- | Executes a new message call immediately without creating a
+-- transaction on the block chain.
+eth_call :: TransactionOptions -> CallMode -> Web3 HexString
+eth_call = remote "eth_call"
+
+-- | Creates new message call transaction or a contract creation,
+-- if the data field contains code.
+eth_sendTransaction :: TransactionOptions -> Web3 HexString
+eth_sendTransaction = remote "eth_sendTransaction"
--- src/Network/Ethereum/Web3/Contract.purs
+++ src/Network/Ethereum/Web3/Contract.purs
@@ -1,43 +1,76 @@
 module Network.Ethereum.Web3.Contract where
 
 import Prelude
-import Control.Monad.Eff.Exception (EXCEPTION)
-import Network.Ethereum.Web3.Encoding (class ABIEncoding, toDataBuilder, fromDataParser)
-import Network.Ethereum.Web3.Types (Web3M, ETH, BigNumber, Address, HexString)
+import Data.Maybe (Maybe(..))
+import Data.Lens ((.~))
+import Control.Monad.Eff.Exception (error)
+import Control.Monad.Error.Class (throwError)
+import Network.Ethereum.Web3.Api (eth_call, eth_sendTransaction)
+import Network.Ethereum.Web3.Encoding (class ABIEncoding, toDataBuilder, fromData)
+import Network.Ethereum.Web3.Types
 
 class ABIEncoding a <= Method a where
     -- | Send a transaction for given contract 'Address', value and input data
-    sendTx :: forall eff .
+    sendTx :: forall e .
               Address
            -- ^ Contract address
            -> BigNumber
            -- ^ paymentValue
            -> a
            -- ^ Method data
-           -> Web3M (eth :: ETH, exception :: EXCEPTION | eff) HexString
+           -> Web3M e HexString
            -- ^ 'Web3' wrapped tx hash
 
     -- | Constant call given contract 'Address' in mode and given input data
-    call :: forall eff b .
+    call :: forall e b .
             ABIEncoding b
          => Address
          -- ^ Contract address
+         -> CallMode
          -- ^ State mode for constant call (latest or pending)
          -> a
          -- ^ Method data
-         -> Web3M (eth :: ETH, exception :: EXCEPTION | eff) b
+         -> Web3M e b
          -- ^ 'Web3' wrapped result
 
---_call :: forall a b eff .
---         ABIEncoding a
---      => ABIEncoding b
---      => Address
---      -> a
---      -> Web3 (eth :: ETH, exception :: EXCEPTION | eff) b
---_call to dat = do
---    res <- eth_call (txdata primeAddress)
---    case fromData (T.drop 2 res) of
---        Nothing -> liftIO $ throwIO $ ParserFail $
---            "Unable to parse result on `" ++ T.unpack res
---            ++ "` from `" ++ show to ++ "`"
---        Just x -> return x
+_sendTransaction :: forall a .
+                    ABIEncoding a
+                 => Maybe Address
+                 -> Address
+                 -> BigNumber
+                 -> a
+                 -> Web3M () HexString
+_sendTransaction mto f val dat =
+    eth_sendTransaction (txdata $ toDataBuilder dat)
+  where
+    defaultGas = parseBigNumber hexadecimal "0x2dc2dc"
+    txdata d =
+      defaultTransactionOptions # _to .~ mto
+                                # _from .~ Just f
+                                # _data .~ Just d
+                                # _value .~ Just val
+                                # _gas .~ defaultGas
+
+_call :: forall a b .
+         ABIEncoding a
+      => ABIEncoding b
+      => Address
+      -> Maybe Address
+      -> CallMode
+      -> a
+      -> Web3M () b
+_call t mf cm dat = do
+    res <- eth_call (txdata <<< toDataBuilder $ dat) cm
+    case fromData res of
+        Nothing -> throwError <<< error $ "Unable to parse result"
+        Just x -> pure x
+  where
+    txdata d  =
+      defaultTransactionOptions # _to .~ Just t
+                                # _from .~ mf
+                                # _data .~ Just d
+
+
+-- data Set = Set Int
+-- instance ABIEncoding Set where
+--   toDataBuilder (Set )
