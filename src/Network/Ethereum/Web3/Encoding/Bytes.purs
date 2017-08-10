2294557307cc7afa5b5264a8df67315b6049ebf0
James Pak <blinky3713@gmail.com>
2017-08-10 00:06:47 -0400

    add size module

--- src/Network/Ethereum/Web3/Encoding/Bytes.purs
+++ src/Network/Ethereum/Web3/Encoding/Bytes.purs
@@ -10,6 +10,7 @@ import Type.Proxy (Proxy(..))
 import Network.Ethereum.Web3.Types (HexString(..), padRight)
 import Node.Encoding (Encoding(Hex))
 import Network.Ethereum.Web3.Encoding.Internal (class EncodingType)
+import Network.Ethereum.Web3.Encoding.Size (class KnownSize, sizeVal)
 
 --------------------------------------------------------------------------------
 -- * Statically sized byte array
@@ -19,14 +20,15 @@ newtype BytesN n = BytesN ByteString
 
 derive newtype instance eqBytesN :: Eq (BytesN n)
 
-update :: forall n . BytesSize n => BytesN n -> ByteString -> BytesN n
+update :: forall n . KnownSize n => BytesN n -> ByteString -> BytesN n
 update _ = BytesN
 
-instance encodingTypeBytes :: BytesSize n => EncodingType (BytesN n) where
-    typeName  = const "bytes[N]"
+instance encodingTypeBytes :: KnownSize n => EncodingType (BytesN n) where
+    typeName  = let n = show (sizeVal (Proxy :: Proxy n))
+                in const $ "bytes[" <> n <> "]"
     isDynamic = const false
 
-instance showNat :: BytesSize n => Show (BytesN n) where
+instance showNat :: KnownSize n => Show (BytesN n) where
     show (BytesN bs) = show <<< HexString $ BS.toString bs Hex
 
 bytesBuilder :: ByteString -> HexString
@@ -35,8 +37,8 @@ bytesBuilder = padRight <<< HexString <<< flip BS.toString Hex
 bytesDecode :: String -> ByteString
 bytesDecode = flip BS.fromString Hex
 
-fromByteString :: forall n . BytesSize n => ByteString -> Maybe (BytesN n)
-fromByteString bs = if BS.length bs > bytesLength (Proxy :: Proxy n)
+fromByteString :: forall n . KnownSize n => ByteString -> Maybe (BytesN n)
+fromByteString bs = if BS.length bs > sizeVal (Proxy :: Proxy n)
                        then Nothing
                        else Just $ BytesN bs
 
@@ -62,56 +64,3 @@ instance encodingTypeBytesD :: EncodingType BytesD where
   typeName  = const "bytes[]"
   isDynamic = const true
 
---------------------------------------------------------------------------------
--- * Type level byte array lengths
---------------------------------------------------------------------------------
-
-data B0
-data B1
-data B2
-data B3
-data B4
-data B5
-data B6
-data B7
-data B8
-data B9
-
-data NumCons a b
-infix 6 type NumCons as :&
-
-class BytesSize n where
-  bytesLength :: Proxy n -> Int
-
-instance bytesSizeB0 :: BytesSize B0 where
-  bytesLength _ = 0
-
-instance bytesSizeB1 :: BytesSize B1 where
-  bytesLength _ = 1
-
-instance bytesSizeB2 :: BytesSize B2 where
-  bytesLength _ = 2
-
-instance bytesSizeB3 :: BytesSize B3 where
-  bytesLength _ = 3
-
-instance bytesSizeB4 :: BytesSize B4 where
-  bytesLength _ = 4
-
-instance bytesSizeB5 :: BytesSize B5 where
-  bytesLength _ = 5
-
-instance bytesSizeB6 :: BytesSize B6 where
-  bytesLength _ = 6
-
-instance bytesSizeB7 :: BytesSize B7 where
-  bytesLength _ = 7
-
-instance bytesSizeB8 :: BytesSize B8 where
-  bytesLength _ = 8
-
-instance bytesSizeB9 :: BytesSize B9 where
-  bytesLength _ = 9
-
-instance bytesSizeCons :: (BytesSize tens, BytesSize ones) => BytesSize (tens :& ones) where
-  bytesLength _ = 10 * (bytesLength (Proxy :: Proxy tens)) + bytesLength (Proxy :: Proxy ones)
