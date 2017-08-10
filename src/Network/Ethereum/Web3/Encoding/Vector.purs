8f6ba0f724fb06107414ab05ca239acd09ab75a7
Martin Allen <martin.allen26@gmail.com>
2017-08-10 01:53:48 -0400

    move vector

--- src/Network/Ethereum/Web3/Encoding/Vector.purs
+++ src/Network/Ethereum/Web3/Encoding/Vector.purs
@@ -1,29 +1,26 @@
 module Network.Ethereum.Web3.Encoding.Vector
   ( Vector,
+    unVector,
     nilVector,
     vCons, (:<),
     vectorLength,
-    toVector,
-    encodeArray
+    toVector
   ) where
 
 import Prelude
-import Data.Array (uncons, length) as A
+import Data.Array (length) as A
 import Data.Array ((:))
 import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Type.Proxy (Proxy(..))
-import Data.Foldable (fold, foldMap)
-import Data.Tuple (Tuple(..), fst, snd)
-import Data.Lens.Index (ix)
-import Data.Lens.Setter (over)
-import Text.Parsing.Parser (fail)
 
 import Network.Ethereum.Web3.Encoding.Size (class KnownNat, S, Z, natVal)
-import Network.Ethereum.Web3.Encoding.AbiEncoding (class ABIEncoding, toDataBuilder)
-import Network.Ethereum.Web3.Types (HexString, hexLength)
+import Network.Ethereum.Web3.Types (HexString)
 
 data Vector n a = Vector (Array a)
+
+unVector :: forall a n . Vector n a -> Array a
+unVector (Vector as) = as
 
 nilVector :: forall a . Vector Z a
 nilVector = Vector mempty
@@ -40,29 +37,3 @@ toVector :: forall a n . KnownNat n => Array a -> Maybe (Vector n a)
 toVector as = if natVal (Proxy :: Proxy n) /= A.length as
                  then Nothing
                  else Just (Vector as)
-
-accumulateOffsets :: Array Int -> Array Int
-accumulateOffsets as = go 0 as
-  where
-    go :: Int -> Array Int -> Array Int
-    go accum bs = case A.uncons bs of
-      Nothing -> []
-      Just ht -> let newAccum = accum + ht.head
-                 in newAccum : go newAccum ht.tail
-
-encodeArray :: forall a .
-               ABIEncoding a
-            => Array a
-            -> HexString
-encodeArray as =
-    let countEncs = map countEnc as
-        offsets = accumulateOffsets <<< over (ix 0) (\x -> x - 32) <<< map fst $ countEncs
-        encodings = map snd countEncs
-    in  foldMap toDataBuilder offsets <> fold encodings
-  where
-    countEnc a = let enc = toDataBuilder a
-                 in Tuple (hexLength enc `div` 2) enc
-
-instance abiEncodingArray :: (ABIEncoding a, KnownNat n) => ABIEncoding (Vector n a) where
-    toDataBuilder (Vector as) = encodeArray as
-    fromDataParser = fail "oops"
