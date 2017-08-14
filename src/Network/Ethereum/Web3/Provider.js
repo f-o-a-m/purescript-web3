0587c541d15d6739ac06724e15e6350b8352c2ee
Martin Allen <martin.allen26@gmail.com>
2017-08-13 22:16:06 -0400

    async tests work

--- src/Network/Ethereum/Web3/Provider.js
+++ src/Network/Ethereum/Web3/Provider.js
@@ -2,19 +2,6 @@
 
 var Web3 = require('web3');
 
-//var getProvider = function () {
-//    var provider;
-//    if (typeof web3 !== 'undefined') {
-//        // Use Mist/MetaMask's provider
-//        provider = web3.currentProvider;
-//    } else {
-//        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-//        provider = new Web3.providers.HttpProvider("http://localhost:8545");
-//    }
-//    console.log(provider);
-//    return provider;
-//
-//};
 
 var getProvider = function () {
     var provider;
@@ -22,7 +9,6 @@ var getProvider = function () {
         // Use Mist/MetaMask's provider
         provider = web3.currentProvider;
     } else {
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         provider = new Web3.providers.HttpProvider("http://localhost:8545");
     }
     return provider;
