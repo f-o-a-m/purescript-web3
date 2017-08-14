40b60cc9fe93483289f3f1734ea3ea6b74dc18b1
James Pak <blinky3713@gmail.com>
2017-08-14 00:41:41 -0400

    make address manditory

--- src/Network/Ethereum/Web3/Provider.js
+++ src/Network/Ethereum/Web3/Provider.js
@@ -1,13 +1,14 @@
 "use strict";
-
 var Web3 = require('web3');
 
 var getProvider = function () {
     var provider;
     if (typeof web3 !== 'undefined') {
         // Use Mist/MetaMask's provider
+        console.log("using metamask provider");
         provider = web3.currentProvider;
     } else {
+        console.log("using http provider");
         provider = new Web3.providers.HttpProvider("http://localhost:8545");
     }
     return provider;
