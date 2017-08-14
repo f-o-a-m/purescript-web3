ae77d9b10cfd99ecba50ecadf4f40279f353173b
James Pak <blinky3713@gmail.com>
2017-08-13 22:44:21 -0400

    window.load

--- src/Network/Ethereum/Web3/Provider.js
+++ src/Network/Ethereum/Web3/Provider.js
@@ -2,6 +2,21 @@
 
 var Web3 = require('web3');
 
+// load it up the first time.
+window.addEventListener('load', function() {
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        console.log('No web3? You should consider trying MetaMask!');
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+    // Now you can start your app & access web3 freely:
+});
 
 var getProvider = function () {
     var provider;
