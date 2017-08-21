227df88e2b825c63238e845d334658118e21e205
Martin Allen <martin.allen26@gmail.com>
2017-08-20 23:24:05 -0400

    added tuple constructors

--- src/Network/Ethereum/Web3/Solidity.purs
+++ src/Network/Ethereum/Web3/Solidity.purs
@@ -12,9 +12,9 @@ import Network.Ethereum.Web3.Solidity.Size (NumCons, type (:&), D0, D1, D2, D3, 
                                             Z, S, N0, N1, N2, N3, N4, N5, N6, N7, N8, N9)
 import Network.Ethereum.Web3.Solidity.Vector (Vector, unVector, nilVector, vCons, (:<), vectorLength, toVector)
 import Network.Ethereum.Web3.Solidity.Bytes (BytesN, unBytesN, proxyBytesN, update, fromByteString)
-import Network.Ethereum.Web3.Solidity.Tuple (Singleton, unSingleton, uncurry1, curry1,
-                                             Tuple2, uncurry2, curry2,
-                                             Tuple3, uncurry3, curry3)
+import Network.Ethereum.Web3.Solidity.Tuple (Singleton(..), unSingleton, uncurry1, curry1,
+                                             Tuple2(..), uncurry2, curry2,
+                                             Tuple3(..), uncurry3, curry3)
 import Network.Ethereum.Web3.Solidity.AbiEncoding (class ABIEncoding, toDataBuilder, fromDataParser)
 import Network.Ethereum.Web3.Types (BigNumber, Address)
 import Data.ByteString (ByteString)
