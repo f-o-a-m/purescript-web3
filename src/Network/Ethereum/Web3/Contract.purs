0d15790a0e081fc0b008aa05a074054e45425eef
Blinky <blinky3713@gmail.com>
2017-12-12 19:36:20 -0800

    clean up and refactor

--- src/Network/Ethereum/Web3/Contract.purs
+++ src/Network/Ethereum/Web3/Contract.purs
@@ -2,29 +2,28 @@ module Network.Ethereum.Web3.Contract where
 
 import Prelude
 
-import Control.Monad.Aff (Fiber, Milliseconds(..), delay)
+import Control.Monad.Aff (Fiber, delay)
 import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff.Exception (error)
 import Control.Monad.Error.Class (throwError)
-import Control.Monad.Reader (ReaderT(..), runReaderT)
-import Data.Array (catMaybes, filter, notElem, singleton)
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Data.Array (catMaybes, notElem)
 import Data.Functor.Tagged (Tagged, untagged)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Eq (genericEq)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Lens ((.~))
-import Data.Machine.Mealy (MealyT, Step(..), halt, mealy, pureMealy, runMealyT, source, wrapEffect)
+import Data.Machine.Mealy (MealyT, Step(..), halt, mealy, runMealy, runMealyT, wrapEffect)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap, unwrap)
-import Data.Profunctor (lmap)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Data.Time.Duration (Milliseconds(..))
 import Data.Traversable (for)
 import Data.Tuple (Tuple(..))
 import Network.Ethereum.Web3.Api (eth_blockNumber, eth_call, eth_getFilterChanges, eth_getLogs, eth_newFilter, eth_sendTransaction, eth_uninstallFilter)
 import Network.Ethereum.Web3.Provider (class IsAsyncProvider, forkWeb3')
-import Network.Ethereum.Web3.Solidity (class DecodeEvent, class GenericABIDecode, class GenericABIEncode, Address, decodeEvent, genericABIEncode, genericFromData)
-import Network.Ethereum.Web3.Types (class EtherUnit, Address, BlockMode(..), BlockNumber, Change(..), ETH, Filter(..), FilterId, HexString, Web3(..), _data, _from, _fromBlock, _gas, _to, _toBlock, _value, convert, defaultTransactionOptions, embed, hexadecimal, parseBigNumber, toSelector)
+import Network.Ethereum.Web3.Solidity (class DecodeEvent, class GenericABIDecode, class GenericABIEncode, decodeEvent, genericABIEncode, genericFromData)
+import Network.Ethereum.Web3.Types (class EtherUnit, Address, BlockMode(..), BlockNumber, Change, ETH, Filter, FilterId, HexString, Web3, _data, _from, _fromBlock, _gas, _to, _toBlock, _value, convert, defaultTransactionOptions, embed, hexadecimal, parseBigNumber, toSelector)
 import Type.Proxy (Proxy(..))
 
 --------------------------------------------------------------------------------
@@ -49,34 +48,16 @@ class EventFilter a where
     -- | Event filter structure used by low-level subscription methods
     eventFilter :: Proxy a -> Address -> Filter
 
-
--- | Start listening to events eminating from the given address and caught by the filter,
--- | using the handler to process the data and decide whether to continue
 event :: forall p e a i ni.
-          IsAsyncProvider p
-       => DecodeEvent i ni a
-       => EventFilter a
-       => Address
-       -> (a -> ReaderT Change (Web3 p e) EventAction)
-       -> Web3 p e (Fiber (eth :: ETH | e) Unit)
+         IsAsyncProvider p
+      => DecodeEvent i ni a
+      => EventFilter a
+      => Address
+      -> (a -> ReaderT Change (Web3 p e) EventAction)
+      -> Web3 p e (Fiber (eth :: ETH | e) Unit)
 event addr handler = do
-    fid <- eth_newFilter (eventFilter (Proxy :: Proxy a) addr)
-    forkWeb3' (Proxy :: Proxy p) $ do
-      loop fid
-      _ <- eth_uninstallFilter fid
-      pure unit
-  where
-    loop :: FilterId -> Web3 p e Unit
-    loop fltr = do
-      _ <- liftAff $ delay (Milliseconds 1000.0)
-      changes <- eth_getFilterChanges fltr
-      acts <- for (catMaybes $ map pairChange changes) $ \(Tuple changeWithMeta changeEvent) -> do
-        runReaderT (handler changeEvent) changeWithMeta
-      when (TerminateEvent `notElem` acts) $ loop fltr
-    pairChange :: Change -> Maybe (Tuple Change a)
-    pairChange rawChange = do
-      change <- decodeEvent rawChange
-      pure (Tuple rawChange change)
+  filterId <- eth_newFilter $ eventFilter (Proxy :: Proxy a) addr
+  forkWeb3' (Proxy :: Proxy p) <<< runMealy $ pollChanges filterId handler
 
 -- | 'runEventsFromBound' will replay events from a lower bound 'BlockNumber'
 -- | making batch requests for changes until it catches up with the chain head,
@@ -85,28 +66,17 @@ runEventsFromBound :: forall p e a i ni.
                       IsAsyncProvider p
                    => DecodeEvent i ni a
                    => EventFilter a
-                   => Proxy a
-                   -> Address
+                   => Address
                    -> BlockNumber
                    -- ^ lower bound block number
                    -> Int
                    -- ^ window size
                    -> (a -> ReaderT Change (Web3 p e) EventAction)
                    -> Web3 p e (Fiber (eth :: ETH | e) Unit)
-runEventsFromBound pa addr bn window handler = do
-    bn <- catchUpEvents pa addr bn window (\a -> ContinueEvent <$ handler a)
-    forkWeb3' $ go (filterChangesStream pa addr bn) unit
-  where
-    go machine s = do
-      a <- runMealyT machine s
-      case a of
-        Halt -> pure unit
-        Emit changes continue -> do
-          acts <- for (catMaybes $ map pairChange changes) $ \(Tuple changeWithMeta changeEvent) -> do
-            runReaderT (handler changeEvent) changeWithMeta
-          if (TerminateEvent `notElem` acts)
-            then go continue s
-            else go halt s
+runEventsFromBound addr leftBound window handler = do
+    let pa = Proxy :: Proxy a
+    bn <- catchUpEvents addr leftBound window (\a -> ContinueEvent <$ handler a)
+    forkWeb3' (Proxy :: Proxy p) $ runMealy (filterChangesStream addr bn handler)
 
 -- | 'catchUpEvents' starts playing the event logs from a starting 'BlockNumber'
 -- | until it has caught up to the latest. It then returns the most recent 'BlockNumber'
@@ -115,19 +85,19 @@ catchUpEvents :: forall p e a i ni.
                  IsAsyncProvider p
               => DecodeEvent i ni a
               => EventFilter a
-              => Proxy a
-              -> Address
+              => Address
               -> BlockNumber
               -- ^ lower bound block number
               -> Int
               -- ^ window size
               -> (a -> ReaderT Change (Web3 p e) EventAction)
               -> Web3 p e BlockNumber
-catchUpEvents pa addr bn w handler =
+catchUpEvents addr bn w handler =
     let s = { currentBlock: bn
             , endingBlock: Latest
             , windowSize: w
             }
+        pa = Proxy :: Proxy a
         eventMachine = leftBoundedFilterStream pa addr
     in go eventMachine s
   where
@@ -137,31 +107,28 @@ catchUpEvents pa addr bn w handler =
         Halt -> pure state.currentBlock
         Emit filter continue -> do
           changes <- eth_getLogs filter
-          acts <- for (catMaybes $ map pairChange changes) $ \(Tuple changeWithMeta changeEvent) -> do
-            runReaderT (handler changeEvent) changeWithMeta
+          acts <- processChanges handler changes
           if (TerminateEvent `notElem` acts)
             then go continue state
             else go halt state {currentBlock = wrap $ unwrap state.currentBlock + one}
 
--- * Event Machines
+-- * Events Streams
 
 -- | 'filterChangesStream' creates a filter from a  lower bound 'BlockNumber' to latest
 -- | then polls the node for changes once per second.
-filterChangesStream :: forall p e a s.
+filterChangesStream :: forall p e a s i ni.
                        IsAsyncProvider p
+                    => DecodeEvent i ni a
                     => EventFilter a
-                    => Proxy a
-                    -> Address
+                    => Address
                     -> BlockNumber
-                    -> MealyT (Web3 p e) s (Array Change)
-filterChangesStream pa addr from = do
-    filterId <- wrapEffect <<< eth_newFilter $ eventFilter pa addr # _fromBlock .~ BN from
-    pollChanges filterId
-  where
-    pollChanges filterId = mealy $ \s -> do
-        liftAff $ delay (Milliseconds 1000.0)
-        changes <- eth_getFilterChanges filterId
-        pure $ Emit changes $ pollChanges filterId
+                    -> (a -> ReaderT Change (Web3 p e) EventAction)
+                    -> MealyT (Web3 p e) s Unit
+filterChangesStream addr from handler = do
+    let pa = Proxy :: Proxy a
+    filterId <- wrapEffect <<< eth_newFilter $ eventFilter pa addr # _fromBlock .~ Just (BN from)
+    pollChanges filterId handler
+
 
 -- Internal Event Helpers
 
@@ -188,7 +155,7 @@ leftBoundedFilterStream pa addr =
     newTo bn current window = min bn ((wrap $ (unwrap current) + embed window))
     succ :: BlockNumber -> BlockNumber
     succ bn = wrap $ unwrap bn + one
-    leftBoundedFilterStream' pa addr' = \s -> do
+    leftBoundedFilterStream' pa' addr' = \s -> do
       end <- case s.endingBlock of
         BN bn -> pure bn
         Earliest -> pure <<< wrap $ zero
@@ -196,16 +163,39 @@ leftBoundedFilterStream pa addr =
       if s.currentBlock > end
          then pure Halt
          else let to' = newTo end s.currentBlock s.windowSize
-                  fltr = eventFilter pa addr'
+                  fltr = eventFilter pa' addr'
                            # _fromBlock .~ (Just <<< BN $ s.currentBlock)
                            # _toBlock .~ (Just $ BN to')
-              in pure $ Emit fltr $ mealy \s ->
-                   leftBoundedFilterStream' pa addr' s {currentBlock = succ to'}
+              in pure $ Emit fltr $ mealy \s' ->
+                   leftBoundedFilterStream' pa' addr' s' {currentBlock = succ to'}
 
-pairChange :: Change -> Maybe (Tuple Change a)
-pairChange rawChange = do
-  change <- decodeEvent rawChange
-  pure (Tuple rawChange change)
+pollChanges :: forall p e a i ni s.
+               IsAsyncProvider p
+            => DecodeEvent i ni a
+            => FilterId
+            -> (a -> ReaderT Change (Web3 p e) EventAction)
+            -> MealyT (Web3 p e) s Unit
+pollChanges filterId handler = mealy $ \s -> do
+    liftAff $ delay (Milliseconds 1000.0)
+    changes <- eth_getFilterChanges filterId
+    acts <- processChanges handler changes
+    if TerminateEvent `notElem` acts
+       then eth_uninstallFilter filterId *> pure Halt
+       else pure $ Emit unit $ pollChanges filterId handler
+
+processChanges :: forall i ni a f.
+                  DecodeEvent i ni a
+               => Monad f
+               => (a -> ReaderT Change f EventAction)
+               -> Array Change
+               -> f (Array EventAction)
+processChanges handler changes = for (catMaybes $ map pairChange changes) \(Tuple changeWithMeta changeEvent) ->
+    runReaderT (handler changeEvent) changeWithMeta
+  where
+    pairChange :: Change -> Maybe (Tuple Change a)
+    pairChange rawChange = do
+      (change :: a) <- decodeEvent rawChange
+      pure (Tuple rawChange change)
 
 --------------------------------------------------------------------------------
 -- * Methods
