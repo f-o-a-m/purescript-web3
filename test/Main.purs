9d23043d33811099df04777f847219882e359b93
Max Kaye <m@xk.io>
2018-02-05 12:19:05 +1100

    Add tests for newtype derivations

--- test/Main.purs
+++ test/Main.purs
@@ -1,17 +1,19 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
-import Web3Spec.Types.Sha3 (sha3Spec)
-import Web3Spec.Types.Utils (utilsSpec)
-import Web3Spec.Types.BigNumber (bigNumberSpec)
-import Web3Spec.Encoding.Simple (encodingSimpleSpec)
-import Web3Spec.Encoding.Generic (encodingGenericSpec)
-import Web3Spec.Encoding.Containers (encodingContainersSpec)
-import Web3Spec.Contract (simpleStorageSpec)
+import Network.Ethereum.Web3.Types (ETH)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
-import Network.Ethereum.Web3.Types (ETH)
+import Web3Spec.Contract (simpleStorageSpec)
+import Web3Spec.Encoding.Containers (encodingContainersSpec)
+import Web3Spec.Encoding.Generic (encodingGenericSpec)
+import Web3Spec.Encoding.Simple (encodingSimpleSpec)
+import Web3Spec.Types.BigNumber (bigNumberSpec)
+import Web3Spec.Types.Newtypes (ntTests)
+import Web3Spec.Types.Sha3 (sha3Spec)
+import Web3Spec.Types.Utils (utilsSpec)
 
 main :: Eff (RunnerEffects (eth :: ETH)) Unit
 main = run [consoleReporter] $ do
@@ -22,4 +24,4 @@ main = run [consoleReporter] $ do
   encodingSimpleSpec
   encodingGenericSpec
   simpleStorageSpec
-
+  ntTests
