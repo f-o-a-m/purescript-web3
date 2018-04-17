20e246920fe071ec9607c1a8dc09cfda5665e37a
blinky3713 <blinky@foam.space>
2018-04-16 23:19:06 -0400

    tests bass when switching to use ethereumjs-utils for everything

--- src/Network/Ethereum/Web3/Types/Sha3.js
+++ src/Network/Ethereum/Web3/Types/Sha3.js
@@ -1,17 +1,5 @@
 "use strict";
 
-var CryptoJS = require('crypto-js');
-var sha3 = require('crypto-js/sha3');
+var EthUtils = require('ethereumjs-util');
 
-exports.sha3StringImpl = function (value) {
-    return sha3(value, {
-        outputLength: 256
-    }).toString();
-};
-
-exports.sha3HexImpl = function (value) {
-    const wordArray = CryptoJS.enc.Hex.parse(value);
-    return sha3(wordArray, {
-        outputLength: 256
-    }).toString();
-};
+exports._sha3 = EthUtils.sha3;
