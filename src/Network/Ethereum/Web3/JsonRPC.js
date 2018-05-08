38158bdc99ce9acd02e08ef5f1ddc6366c704b12
Irakli Safareli <i.safareli@gmail.com>
2018-05-08 17:41:09 +0200

    no need for canceling flag

--- src/Network/Ethereum/Web3/JsonRPC.js
+++ src/Network/Ethereum/Web3/JsonRPC.js
@@ -4,18 +4,14 @@
 exports._sendAsync = function (provider) {
     return function (request) {
         return function(onError, onSuccess) {
-            var canceled = false;
             provider.sendAsync(request, function(err, succ) {
-                if (canceled) {
-                    return;
-                } else if (err) {
+                if (err) {
                     onError(err);
                 } else {
                     onSuccess(succ);
                 }
             });
             return function (cancelError, onCancelerError, onCancelerSuccess) {
-                canceled = true;
                 onCancelerSuccess();
             };
         };
