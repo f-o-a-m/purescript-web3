f3949d9f2fbd93ce27fc2bcb34e6317829078220
Irakli Safareli <i.safareli@gmail.com>
2018-05-08 16:37:08 +0200

    fix _sendAsync canceler
    HttpProvider.prototype.sendAsync returns undefined so we can't cancel it.
    https://github.com/ethereum/web3.js/blob/develop/lib/web3/httpprovider.js#L114-L141

--- src/Network/Ethereum/Web3/JsonRPC.js
+++ src/Network/Ethereum/Web3/JsonRPC.js
@@ -4,15 +4,18 @@
 exports._sendAsync = function (provider) {
     return function (request) {
         return function(onError, onSuccess) {
-            var cancel = provider.sendAsync(request, function(err, succ) {
-                if (err) {
+            var canceled = false;
+            provider.sendAsync(request, function(err, succ) {
+                if (canceled) {
+                    return;
+                } else if (err) {
                     onError(err);
                 } else {
                     onSuccess(succ);
                 }
             });
             return function (cancelError, onCancelerError, onCancelerSuccess) {
-                cancel();
+                canceled = true;
                 onCancelerSuccess();
             };
         };
