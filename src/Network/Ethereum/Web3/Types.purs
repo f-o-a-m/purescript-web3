70e5839b0ec9d44aacdb757538c4289fce7c9653
blinky3713 <blinky3713@gmail.com>
2018-05-24 11:14:18 -0400

    added formatValue

--- src/Network/Ethereum/Web3/Types.purs
+++ src/Network/Ethereum/Web3/Types.purs
@@ -6,5 +6,5 @@ module Network.Ethereum.Web3.Types
   ) where
 
 import Network.Ethereum.Types (Address, BigNumber, HexString, embed, mkAddress, mkHexString, unAddress, unHex)
-import Network.Ethereum.Web3.Types.EtherUnit (class EtherUnit, Babbage, Ether, Finney, KEther, Lovelace, NoPay, Shannon, Szabo, Value, Wei, convert, fromWei, mkValue, toWei)
+import Network.Ethereum.Web3.Types.EtherUnit (class EtherUnit, Babbage, Ether, Finney, KEther, Lovelace, NoPay, Shannon, Szabo, Value, Wei, convert, formatValue, fromWei, mkValue, toWei)
 import Network.Ethereum.Web3.Types.Types (forkWeb3, forkWeb3', runWeb3, Block(..), BlockNumber(..), CallError(..), ChainCursor(..), Change(..), ETH, EventAction(..), FalseOrObject(..), Filter, FilterId, MethodName, Request, Response(..), RpcError(..), SyncStatus(..), Transaction(..), TransactionOptions(..), TransactionReceipt(..), TransactionStatus(..), Web3, Web3Par, Web3Error(..), _address, _data, _from, _fromBlock, _gas, _gasPrice, _nonce, _to, _toBlock, _topics, _value, defaultFilter, defaultTransactionOptions, mkRequest, throwWeb3)
