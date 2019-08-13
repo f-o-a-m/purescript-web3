4d901423cee673fcb5aa1387c8bdd151f31d30b5
Martin Allen <blinky@foam.space>
2019-08-13 15:02:19 -0700

    remove old internal module

--- src/Network/Ethereum/Web3/Contract/Events.purs
+++ src/Network/Ethereum/Web3/Contract/Events.purs
@@ -11,8 +11,8 @@ module Network.Ethereum.Web3.Contract.Events
 
 import Prelude
 
-import Control.Coroutine (Producer, Consumer, Process, producer, consumer, pullFrom)
-import Control.Coroutine.Transducer (Transducer, awaitForever, toProducer, yieldT, (=>=))
+import Control.Coroutine (Consumer, Process, producer, consumer, pullFrom)
+import Control.Coroutine.Transducer (Transducer, awaitForever, fromProducer, toProducer, yieldT, (=>=))
 import Control.Monad.Fork.Class (bracket)
 import Control.Monad.Reader.Trans (ReaderT, runReaderT)
 import Control.Monad.Rec.Class (class MonadRec)
@@ -24,8 +24,8 @@ import Data.Lens ((.~), (^.))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap, unwrap)
 import Data.Time.Duration (Milliseconds(..))
-import Data.Traversable (for, for_)
-import Data.Tuple (Tuple(..), fst)
+import Data.Traversable (for_)
+import Data.Tuple (fst)
 import Effect.Aff (delay)
 import Effect.Aff.Class (liftAff)
 import Network.Ethereum.Core.BigNumber (BigNumber, embed)
@@ -36,20 +36,41 @@ import Network.Ethereum.Web3.Types (EventAction(..), BlockNumber, ChainCursor(..
 
 --import Debug.Trace (traceM)
 --------------------------------------------------------------------------------
+-- * Types
+--------------------------------------------------------------------------------
 
-  -- * Filter Stream
+type EventHandler f e = e -> ReaderT Change f EventAction
 
-type FilterStreamState a =
+type FilterStreamState e =
   { currentBlock :: BlockNumber
-  , initialFilter :: Filter a
+  , initialFilter :: Filter e
   , windowSize :: Int
   }
 
-produceFilter
+type FilterChange e =
+  { rawChange :: Change
+  , event :: e
+  }
+
+type ChangeReceipt =
+  { logIndex :: BigNumber
+  , blockHash :: HexString
+  , blockNumber :: BlockNumber
+  , action :: EventAction
+  }
+
+--------------------------------------------------------------------------------
+-- * Event Coroutines
+--------------------------------------------------------------------------------
+
+-- | Taking an initial state, create a stream of filters used for querying event logs.
+-- | The coroutine terminates when it has read up to the `toBlock` field, yielding
+-- | the current state.
+filterProducer
   :: forall e.
      FilterStreamState e
   -> Transducer Void (Filter e) Web3 (FilterStreamState e)
-produceFilter currentState = do
+filterProducer currentState = do
     let initialToBlock = currentState.initialFilter ^. _toBlock
     --traceM ("[produceFilter] inititailToBlock=" <> show initialToBlock)
     end <- lift <<< mkBlockNumber $ initialToBlock
@@ -60,7 +81,7 @@ produceFilter currentState = do
           continue = do
             --traceM "Waiting For Blocks..."
             lift $ liftAff $ delay (Milliseconds 3000.0)
-            produceFilter currentState
+            filterProducer currentState
         in
           case initialToBlock of
              Pending -> continue
@@ -72,34 +93,58 @@ produceFilter currentState = do
                      # _fromBlock .~ BN currentState.currentBlock
                      # _toBlock .~ BN to'
         yieldT fltr
-        produceFilter $ currentState {currentBlock = succ to'}
+        filterProducer currentState { currentBlock = succ to' }
   where
     newTo :: BlockNumber -> BlockNumber -> Int -> BlockNumber
     newTo upper current window = min upper (wrap $ unwrap current + embed window)
     succ :: BlockNumber -> BlockNumber
     succ bn = wrap $ unwrap bn + one
 
+-- | Taking in a stream of filters, produce a stream of `FilterChange`s from querying
+-- | the getLogs method.
 makeFilterChanges
   :: forall i ni e.
      DecodeEvent i ni e
-  => Transducer (Filter e) (FilterChange e) Web3 Unit
+  => Transducer (Filter e) (Array (FilterChange e)) Web3 Unit
 makeFilterChanges = awaitForever \fltr -> do
   --traceM $ "[makeFilterChanges] fltr : " <> show fltr
   changes <- lift $ eth_getLogs fltr
   --traceM $ "[makeFilterChanges] changes : " <> show changes
-  for_ (mkFilterChanges changes) yieldT
+  yieldT $ mkFilterChanges changes
 
+-- | A stateless (on the server) stream of filter changes starting from an initial
+-- | filter.
 logsStream
   :: forall i ni e.
      DecodeEvent i ni e
   => FilterStreamState e
   -> Transducer Void (FilterChange e) Web3 (FilterStreamState e)
 logsStream initialState =
-  fst <$> (produceFilter initialState =>= makeFilterChanges)
+  fst <$> (filterProducer initialState =>= stagger makeFilterChanges)
+
+-- | `pollFilter` takes a `FilterId` and a max `ChainCursor` and polls a filter
+-- | for changes until the chainHead's `BlockNumber` exceeds the `ChainCursor`,
+-- | if ever. There is a minimum delay of 1 second between polls.
+pollFilter
+  :: forall a i ni .
+     DecodeEvent i ni a
+  => FilterId
+  -> ChainCursor
+  -> Transducer Void (Array (FilterChange a)) Web3 BlockNumber
+pollFilter filterId stop = fromProducer $ producer $ do
+  bn <- eth_blockNumber
+  if BN bn > stop
+     then do
+       pure <<< Right $ bn
+     else do
+       liftAff $ delay (Milliseconds 1000.0)
+       changes <- eth_getFilterChanges filterId
+       pure <<< Left $ mkFilterChanges changes
+
 
 --------------------------------------------------------------------------------
-
-type EventHandler f e = e -> ReaderT Change f EventAction
+-- * Utils
+--------------------------------------------------------------------------------
 
 -- | `reduceEventStream` takes a handler and an initial state and attempts to run
 -- | the handler over the event stream. If the machine ends without a `TerminateEvent`
@@ -135,34 +180,6 @@ aquireFilter fltr h =
       withFilter = void <<< h
   in bracket aquire onRelease withFilter
 
--- | `pollFilter` takes a `FilterId` and a max `ChainCursor` and polls a filter
--- | for changes until the chainHead's `BlockNumber` exceeds the `ChainCursor`,
--- | if ever. There is a minimum delay of 1 second between polls.
-pollFilter
-  :: forall a i ni .
-     DecodeEvent i ni a
-  => FilterId
-  -> ChainCursor
-  -> Producer (Array (FilterChange a)) (Web3) BlockNumber
-pollFilter filterId stop = producer $ do
-  bn <- eth_blockNumber
-  if BN bn > stop
-     then do
-       pure <<< Right $ bn
-     else do
-       liftAff $ delay (Milliseconds 1000.0)
-       changes <- eth_getFilterChanges filterId
-       pure <<< Left $ mkFilterChanges changes
-
---------------------------------------------------------------------------------
-
--- * Process Filter Changes helpers
-
-type FilterChange a =
-  { rawChange :: Change
-  , event :: a
-  }
-
 mkFilterChanges
   :: forall i ni a .
      DecodeEvent i ni a
@@ -175,13 +192,6 @@ mkFilterChanges cs = catMaybes $ map pairChange cs
      pure { rawChange : rawChange
           , event : a
           }
-
-type ChangeReceipt =
-  { logIndex :: BigNumber
-  , blockHash :: HexString
-  , blockNumber :: BlockNumber
-  , action :: EventAction
-  }
 
 processChange
   :: forall e f.
@@ -197,21 +207,20 @@ processChange handler c@{rawChange: Change change} = do
        , action
        }
 
-processChanges
-  :: forall e f.
-     Monad f
-  => EventHandler f e
-  -> Array (FilterChange e)
-  -> f (Array (Tuple EventAction BlockNumber))
-processChanges handler changes = for changes \c -> do
-    act <- runReaderT (handler c.event) c.rawChange
-    let (Change change) = c.rawChange
-    pure $ Tuple act change.blockNumber
-
-
 -- | Coerce a 'ChainCursor' to an actual 'BlockNumber'.
 mkBlockNumber :: ChainCursor -> Web3 BlockNumber
 mkBlockNumber bm = case bm of
   BN bn -> pure bn
   Earliest -> pure <<< wrap $ zero
   _ -> eth_blockNumber
+
+stagger
+  :: forall i o m a par.
+     Monad m
+  => MonadRec m
+  => Parallel par m
+  => Transducer i (Array o) m a
+  -> Transducer i o m a
+stagger osT =
+  let trickle = awaitForever \os -> for_ os yieldT
+  in fst <$> (osT =>= trickle)
