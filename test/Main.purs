8305b9a24de8d9fd5ccb247856ce021442b7116a
Martin Allen <blinky@foam.space>
2019-08-14 11:16:27 -0700

    bumped purescript-spec version

--- test/Main.purs
+++ test/Main.purs
@@ -2,25 +2,30 @@ module Test.Main where
 
 import Prelude
 
+import Data.Identity (Identity(..))
 import Data.Maybe (Maybe(..))
+import Data.Newtype (un)
 import Effect (Effect)
+import Effect.Aff (Milliseconds(..), launchAff_)
+import Network.Ethereum.Web3.Types.Provider (httpProvider)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run', defaultConfig)
+import Test.Spec.Runner (defaultConfig, runSpecT)
 import Web3Spec.Contract (simpleStorageSpec)
 import Web3Spec.Encoding.Containers (encodingContainersSpec)
+import Web3Spec.Encoding.DataSpec (dataMakerSpec)
 import Web3Spec.Encoding.Generic (encodingGenericSpec)
 import Web3Spec.Encoding.Simple (encodingSimpleSpec)
 import Web3Spec.EtherUnitSpec (etherUnitTests)
-import Web3Spec.Types.Vector (vectorSpec)
 import Web3Spec.Live.LiveSpec (liveSpec)
-import Web3Spec.Encoding.DataSpec (dataMakerSpec)
-import Network.Ethereum.Web3.Types.Provider (httpProvider)
+import Web3Spec.Types.Vector (vectorSpec)
 
 
 main :: Effect Unit
 main = do
+  let cfg =  defaultConfig {timeout = Just (Milliseconds $ 60.0 * 1000.0)}
+      runner = void <<< un Identity <<< runSpecT cfg [consoleReporter]
   p <- httpProvider "http://localhost:8545"
-  run' defaultConfig {timeout = Just (60 * 1000)} [consoleReporter] $ do
+  launchAff_ $ runner do
     dataMakerSpec
     vectorSpec
     encodingContainersSpec
