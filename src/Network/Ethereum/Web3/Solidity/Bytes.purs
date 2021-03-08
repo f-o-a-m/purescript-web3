d199533d29fff082e38c3fbbd38d957d16f3267f
Martin <martin@foam.space>
2021-03-07 20:43:45 -0800

    fix bytesN

--- src/Network/Ethereum/Web3/Solidity/Bytes.purs
+++ src/Network/Ethereum/Web3/Solidity/Bytes.purs
@@ -7,13 +7,10 @@ module Network.Ethereum.Web3.Solidity.Bytes
   ) where
 
 import Prelude
-import Data.ByteString (empty, ByteString, Encoding(Hex))
-import Data.ByteString as BS
-import Data.Maybe (Maybe(..), fromJust)
+import Data.ByteString (ByteString)
+import Data.Maybe (Maybe(..))
 import Network.Ethereum.Core.HexString as Hex
-import Network.Ethereum.Types (mkHexString)
 import Network.Ethereum.Web3.Solidity.Size (class KnownSize, DLProxy(..), sizeVal, kind DigitList)
-import Partial.Unsafe (unsafePartial)
 
 --------------------------------------------------------------------------------
 -- * Statically sized byte array
@@ -21,33 +18,31 @@ import Partial.Unsafe (unsafePartial)
 -- Represents a statically sized bytestring of size `n` bytes.
 -- | See module [Network.Ethereum.Web3.Solidity.Sizes](/Network.Ethereum.Web3.Solidity.Sizes) for some predefined sizes.
 newtype BytesN (n :: DigitList)
-  = BytesN ByteString
+  = BytesN Hex.HexString
 
 derive newtype instance eqBytesN :: Eq (BytesN n)
 
-instance showBytesN :: KnownSize n => Show (BytesN n) where
-  show (BytesN bs) = show <<< unsafePartial fromJust <<< mkHexString $ BS.toString bs Hex
+derive newtype instance showBytesN :: Show (BytesN n)
 
 derive newtype instance ordBytesN :: Ord (BytesN n)
 
 -- | Access the underlying raw bytestring
 unBytesN :: forall n. KnownSize n => BytesN n -> ByteString
-unBytesN (BytesN bs) = bs
+unBytesN (BytesN bs) = Hex.toByteString bs
 
 proxyBytesN :: forall n. KnownSize n => BytesN n
-proxyBytesN = BytesN empty
+proxyBytesN = BytesN mempty
 
 update :: forall n. KnownSize n => BytesN n -> ByteString -> BytesN n
-update _ = BytesN
+update _ = BytesN <<< Hex.fromByteString
 
 -- | Attempt to coerce a bytestring into one of the appropriate size.
 -- | See module [Network.Ethereum.Web3.Solidity.Sizes](/Network.Ethereum.Web3.Solidity.Sizes) for some predefined sizes.
 fromByteString :: forall n. KnownSize n => DLProxy n -> ByteString -> Maybe (BytesN n)
 fromByteString _ bs =
-  if not $ BS.length bs <= sizeVal (DLProxy :: DLProxy n) then
+  if not $ (Hex.hexLength h / 2) <= sizeVal (DLProxy :: DLProxy n) then
     Nothing
   else
-    let
-      padded = Hex.toByteString <<< Hex.padLeft <<< Hex.fromByteString $ bs
-    in
-      Just $ BytesN padded
+    Just $ BytesN h
+  where
+  h = Hex.fromByteString bs
