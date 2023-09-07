14f0f42e22033264f4651b21493e19be00cd7db8
Martin Allen <31280145+martyall@users.noreply.github.com>
2023-09-06 22:41:11 -0700

    remove unused live tests (#165)
    * remove unused live tests
    
    * update dhall files
    
    * tidy

--- test/web3/Main.purs
+++ test/web3/Main.purs
@@ -8,21 +8,14 @@ import Effect (Effect)
 import Effect.Aff (Aff, Milliseconds(..), launchAff_)
 import Effect.Class (liftEffect)
 import Network.Ethereum.Web3.Types.Provider (httpProvider)
-import Test.Spec (Spec, SpecT, parallel, mapSpecTree)
+import Test.Spec (Spec, SpecT, mapSpecTree)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (defaultConfig, runSpecT)
 import Web3Spec.Encoding.ContainersSpec as EncodingContainersSpec
 import Web3Spec.Encoding.DataSpec as EncodingDataSpec
 import Web3Spec.Encoding.GenericSpec as EncodingGenericSpec
 import Web3Spec.Encoding.SimpleSpec as EncodingSimpleSpec
-import Web3Spec.Live.SimpleStorageSpec as SimpleStorageSpec
-import Web3Spec.Live.ComplexStorageSpec as ComplexStorageSpec
-import Web3Spec.Live.MockERC20Spec as MockERC20Spec
-import Web3Spec.Live.PayableTestSpec as PayableTestSpec
-import Web3Spec.Live.SimpleErrorTestSpec as SimpleErrorTestSpec
-import Web3Spec.Live.MultifilterSpec as MultifilterSpec
 import Web3Spec.Live.RPCSpec as RPCSpec
-import Web3Spec.Live.FilterSpec as FilterSpec
 import Web3Spec.Types.EtherUnitSpec as EtherUnitSpec
 import Web3Spec.Types.VectorSpec as VectorSpec
 
@@ -42,18 +35,6 @@ main =
             EncodingGenericSpec.spec
             EtherUnitSpec.spec
           RPCSpec.spec p
-          FilterSpec.spec p
-          -- payable spec can't be run in parallel :/
-          PayableTestSpec.spec p
-          -- all of these tests only have one `it` statement and
-          -- are dealing with separate contracts so they can be run
-          -- in parallel
-          parallel do
-            SimpleStorageSpec.spec p
-            ComplexStorageSpec.spec p
-            MockERC20Spec.spec p
-            SimpleErrorTestSpec.spec p
-            MultifilterSpec.spec p
   where
   hoist :: Spec ~> SpecT Aff Unit Aff
   hoist = mapSpecTree (pure <<< un Identity) identity
